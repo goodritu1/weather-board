8d1402243c1cb789ee2ba15dc166fae81a60cc1f
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-05-06 13:49:17 -0700

    fixed the API url but the 5 days url is still not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,44 @@
 const searchButton = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
-const API_Key = "6498761679a8651b9f26835393d6936";
-const getDetails=(cityName,lat,lon) =>{
- const fiveDaysApi= 'http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={API_Key}';
- fetcg(fiveDaysApi)
+const API_Key = "6498761679a8651b9f26835393d69362";
+
+const getDetails=(data) =>{
+    const lat =data.lat;
+    console.log (lat);
+    const lon =data.lat;
+    console.log (lon);
+
+ const fiveDaysApi= `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_Key}`;
+ fetch(fiveDaysApi)
  .then(function (response){
     if(!response.ok) throw response;
     return response.json();
  })
  .then(function(data){
-    console.log(data);  
+    
+   
+    const forecastDays =[];
+    const fiveDaysForecast= data.list.filter(forecast => {
+        const forecastDate =new Date (forecast.dt.txt).getDate();
+if (!forecastDays.includes(forecastDate)){
+    return forecastDays.push(forecastDate);
+}
+    });
+    console.log(fiveDaysForecast);
  }) .catch (function(error){
   alert("error");
  })
 }
+
 const getCoordinates =function() {
     const cityName = cityInput.value.trim();
     if(!cityName)
    
     return;
     console.log(cityName);
-    const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit={limit}&appid=${API_Key}';
-   
+    const limit = 5;
+    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${API_Key}`;
+   console.log(apiUrl);
     fetch(apiUrl)
     .then(function (response) {
         if(!response.ok) 
@@ -29,17 +46,19 @@ const getCoordinates =function() {
     return response.json();
     })
     .then(function(data){
+        console.log("new data", data);
         if(!data.length) throw new Error('no coordinates found');
-        const [name, lat,lon]= data[0];
 
-        console.log(name, lat,lon);
-        getDetails(name, lat,lon);
+        // const [name,lat,lon]= data[0];
+
+        
+        getDetails(data[0]);
     })
     .catch(function(error){
         if (error.message ==='no coordinates found'){
-        alert("error");
+        alert("error!! no coordinate found");
     }else { 
-        alert("error");
+        console.log("error", error);
                 }
             });
 
