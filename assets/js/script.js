890ae4ada68ad703154ef8f78996a8ef851701fa
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-05-09 18:15:16 -0700

    fixed cards and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,134 +8,115 @@ const API_Key = "6498761679a8651b9f26835393d69362";
 const createWeatherCard = (cityName, weatherItem, index) => {
     if (index === 0) {
         return ` <div class ="details">
-         <h2> ${cityName}(${weatherItem.dt_txt.split(" ")[0]})</h2>
-        <h4>Temp: ${(weatherItem.main.temp - 278.73).toFixed(2)}</h4>
+         <h2> ${cityName}(${weatherItem.dt_txt})</h2>
+        <h4>Temp: ${(weatherItem.main.temp).toFixed(2)}</h4>
         <h4>Wind:${weatherItem.wind.speed} M/S </h4>
         <h4>Humidity: ${weatherItem.main.humidity}%</h4>
       </div>
       <div class ="image">
-        <img src ="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt ="forecast">
+        <img src ="https://openweathermap.org/img/w/${weatherItem.weather[0].icon}.png" alt ="forecast">
         <h4> ${weatherItem.weather[0].description}</h4>
       </div>`;
     } else {
         return `<li class="card">
-      <h3> (${weatherItem.dt_txt.split("")[0]})</h3>
-      <img src ="https://openweather.org/img/wn/${weatherItem.weather[0].icon}@2x.png">
-        <h4>Temp: ${(weatherItem.main.temp - 278.73).toFixed(2)} </h4>
+      <h3> (${weatherItem.dt_txt})</h3>
+      <img src ="https://openweathermap.org/img/w/${weatherItem.weather[0].icon}.png">
+        <h4>Temp: ${(weatherItem.main.temp).toFixed(2)} </h4>
         <h4>Wind:${weatherItem.wind.speed} M/S </h4>
         <h4>Humidity: ${weatherItem.main.humidity}%</h4>
         </li>`;
     }
 }
-    const getDetails = (cityName, lat, lon) => {
-        // const lat =data.lat;
-        // console.log (lat);
-        // const lon =data.lat;
-        // console.log (lon);
+const getDetails = (cityName, lat, lon) => {
+    // const lat =data.lat;
+    // console.log (lat);
+    // const lon =data.lat;
+    // console.log (lon);
 
-        const fiveDaysApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_Key}`;
-        // the API returns forecast for every 3 hrs for 5 days.
-        fetch(fiveDaysApi)
-            .then(res => res.json())
-            .then(data => {
-                // we need to filter to only one forecast everyday
-                const forecastDays = [];
-                const fiveDaysForecast = data.list.filter(forecast => {
-                    const forecastDate = new Date(forecast.dt_txt).getDate();
-                    if (!forecastDays.includes(forecastDate)) {
-                        return forecastDays.push(forecastDate);
-                    }
-                });
-                // we have to clear previos data
-                cityInput.value = "";
-                forecastCardDiv.innerHTML = "";
-                currentWeatherDiv.innerHTML = "";
-
-                // console.log(fiveDaysForecast);
-                fiveDaysForecast.forEach((weatherItem, index) => {
-                    if (index === 0) {
-                        currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
-                    } else {
-                        forecastCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
-                    }
-                    // adding weather cards to the page
-                    forecastCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
-                }).catch(() => {
-                    alert("an error occured while fetchin the coordinates!");
-                });
+    const fiveDaysApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_Key}&units=imperial`;
+    // the API returns forecast for every 3 hrs for 5 days.
+    fetch(fiveDaysApi)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.list);
+            // we need to filter to only one forecast everyday
+            const forecastDays = [];
+            const fiveDaysForecast = data.list.filter(forecast => {
+                return forecast.dt_txt.includes("12:00:00");
+              
             });
-    }
-    //  .then(function (response){
-    //     if(!response.ok) throw response;
-    //     return response.json();
-    //  })
-    //  .then(function(data){
+            // we have to clear previos data
+            console.log(fiveDaysForecast);
+            cityInput.value = "";
+            forecastCardDiv.innerHTML = "";
+            currentWeatherDiv.innerHTML = "";
+
+            // console.log(fiveDaysForecast);
+            fiveDaysForecast.forEach((weatherItem, index) => {
+                if (index === 0) {
+                    currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                } else {
+                    forecastCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                }
+                // adding weather cards to the page
+                // forecastCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            })
+        }).catch(() => {
+            alert("an error occured while fetchin the coordinates!");
+        });
+}
+//  .then(function (response){
+//     if(!response.ok) throw response;
+//     return response.json();
+//  })
+//  .then(function(data){
 
 
-    //     const forecastDays =[];
-    //     const fiveDaysForecast= data.list.filter(forecast => {
-    //         const forecastDate =new Date (forecast.dt_txt).getDate();
-    // if (!forecastDays.includes(forecastDate)){
-    //     return forecastDays.push(forecastDate);
-    // }
-    //     });
+//     const forecastDays =[];
+//     const fiveDaysForecast= data.list.filter(forecast => {
+//         const forecastDate =new Date (forecast.dt_txt).getDate();
+// if (!forecastDays.includes(forecastDate)){
+//     return forecastDays.push(forecastDate);
+// }
+//     });
 
 
-    //     console.log(fiveDaysForecast);
-    //     fiveDaysForecast.forEach(weatherItem => {
-    //         forecastCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem))
+//     console.log(fiveDaysForecast);
+//     fiveDaysForecast.forEach(weatherItem => {
+//         forecastCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem))
 
-    //     })
-    //  }) .catch (function(error){
-    //   alert("error");
-    //  })
-    // }
+//     })
+//  }) .catch (function(error){
+//   alert("error");
+//  })
+// }
 
-    const getCoordinates = function () {
-        const cityName = cityInput.value.trim();
-        if (!cityName)
+const getCoordinates = function () {
+    const cityName = cityInput.value.trim();
+    if (!cityName)
 
-            return;
-        console.log(cityName);
-        const limit = 5;
-        const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${API_Key}`;
-        //    console.log(apiUrl);
+        return;
+    console.log(cityName);
+    const limit = 5;
+    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${API_Key}`;
+    //    console.log(apiUrl);
 
-        fetch(apiUrl).then(res => res.json()).then(data => {
-            if (!data.length) return alert(`no coordinates found for ${cityName}`);
-            const { name, lat, lon } = data[0];
-            getDetails(name, lat, lon);
-        })
-            .catch(() => {
-                alert("And error occured while fetching the coordinates!");
-            });
-        // fetch(apiUrl)
-        // .then(function (response) {
-        //     if(!response.ok) 
-        //     throw response;
-        // return response.json();
-        // })
-        // .then(function(data){
-        //     console.log("new data", data);
-        //     if(!data.length) throw new Error('no coordinates found');
-
-        //     // const [name,lat,lon]= data[0];
-
-
-        //     getDetails(data[0]);
-        // })
-        // .catch(function(error){
-        //     if (error.message ==='no coordinates found'){
-        //     alert("error!! no coordinate found");
-        // }else { 
-        //     console.log("error", error);
-        //             }
-        //         });
-
-
-    }
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        if (!data.length) return alert(`no coordinates found for ${cityName}`);
+        console.log(data);
+        const { name, lat, lon } = data[0];
+        getDetails(name, lat, lon);
+    })
+        .catch(() => {
+            alert("And error occured while fetching the coordinates!");
+        });
 
 
 
-    searchButton.addEventListener("click", getCoordinates);
+
+}
+
+
+
+searchButton.addEventListener("click", getCoordinates);
 
